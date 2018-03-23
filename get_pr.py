44125fcbc578b475db03769d0ebda21fe86ec925
apc-kamibayashi <kamibayashi@ap-com.co.jp>
2018-03-23 20:14:55 +0900

    オープンしているPRがない場合にも対応できるように変更

--- get_pr.py
+++ get_pr.py
@@ -1,5 +1,3 @@
-#test
-
 import sys
 import json
 import requests
@@ -22,7 +20,7 @@ scope = 'repo'#APIから取る値をリスト型で定義しておく
 #その他の利用ファイルなどの定義
 now = datetime.datetime.now()
 file_name = 'get_pr_{0:%Y%m%d-%H%M%S}.csv'.format(now)
-api_param = {
+api_param = { #取りたいAPIパラメータのカラム名定義と取る値定義
     u'No.':'number',
     u'プルリク名':'title',
     u'実施者':['user','login'], #取りたいJSONが二段の場合リストで定義。今のところ二段まで。
@@ -32,21 +30,13 @@ api_param = {
     u'コメント数':'comments',
     u'コミット数':'commits',
     u'マージ':'merged',
-    u'ファイル変更数':'changed_files'
+    u'ファイル変更数':'changed_files' #ファイル変更数がある場合のみ別の関数が走るため、後ろ推奨
 }
 
-#CSVファイルのcolomnを作る
-if os.path.exists(file_name):
-    os.remove(file_name)
-
-with open(file_name, 'a') as f:
-    for i,key in enumerate(api_param):
-        #print(i)
-        f.write(key)
-        if i < len(api_param) - 1:
-            f.write(',')
-    f.write('\n')
-f.closed
+file_param = { #取りたいファイル変更分パラメータのカラム名定義と取る値定義
+    u'追加': 'additions',
+    u'削除': 'deletions'
+}
 
 #アクセストークンを取得
 # 認証URLをブラウザに打ってリダイレクトURLのcodeを入力
@@ -63,17 +53,46 @@ data = res.text.split('&')
 access_token = data[0].split('=')[1] #文字列の「access_token=」を削除
 #print('access_token: '+ access_token) #エラー確認用に一時的に出力
 
-#プルリクの最大数を定義
-github_url = 'https://api.github.com/repos/%s/%s/pulls' % (owner,repo)
-payload={'access_token':access_token}
-res = requests.get(github_url,params=payload)
-pr_list_json_data = json.loads(res.text) #なぜかこのPRはリスト型のため注意
-#print(pr_list_json_data) #エラー確認用に一時的に出力
-max_pr = pr_list_json_data[0]['number'] #numberはint型で取れる
+#プルリクの最大数を定義ver2：オープンがなくても抜けられる
+for i in range(1,65535): #とりあえず65535くらいに設定
+    #print(i) #エラー確認用に一時的に出力
+    github_url = 'https://api.github.com/repos/%s/%s/pulls/' % (owner,repo) + str(i)
+    res = requests.get(github_url,params=payload)
+    pr_json_data = json.loads(res.text) #個別PRのjson_dataは辞書型
+    #print(pr_json_data) #エラー確認用に一時的に出力
+    if ('message' in pr_json_data) == 1:
+        max_pr = i - 1
+        break
+
+#columnのためにchanged_filesの最大数をとる
+for i in range(1,65535): #とりあえず65535くらいに設定
+    #print(i) #エラー確認用に一時的に出力
+    github_url = 'https://api.github.com/repos/%s/%s/pulls/' % (owner,repo) + str(i)
+    res = requests.get(github_url,params=payload)
+    pr_json_data = json.loads(res.text) #個別PRのjson_dataは辞書型
+    #print(pr_json_data) #エラー確認用に一時的に出力
+    if ('message' in pr_json_data) == 1:
+        max_pr = i - 1
+        break
 
 #テスト用にファイルに出力
 #f = open("output.json", "w")
 #json.dump(pr_list_json_data, f, indent=4, sort_keys=True, separators=(',', ': '))
+
+#CSVファイルのcolomnを作る
+if os.path.exists(file_name):
+    os.remove(file_name)
+
+with open(file_name, 'a') as f:
+    for i,key in enumerate(api_param):
+        #print(i)
+        f.write(key)
+        if i < len(api_param) - 1:
+            f.write(',')
+    f.write('\n')
+f.closed
+
+str = ''
 
 #プルリクエストを取得
 for i in range(1,max_pr+1):
@@ -91,24 +110,35 @@ for i in range(1,max_pr+1):
             #print(api_param[value]) #エラー確認用に一時的に出力
             if isinstance(api_param[value],list) == 1: #取りたいJSONが二段だった場合(二段まで対応)
                 #print(pr_json_data[api_param[value][0]][api_param[value][1]]) #エラー確認用に一時的に出力
-                f.write(pr_json_data[api_param[value][0]][api_param[value][1]])
+                str = str + pr_json_data[api_param[value][0]][api_param[value][1]]
             elif isinstance(pr_json_data[api_param[value]],int) == 1: #取ったデータが整数だった場合
                 #print(pr_json_data[api_param[value]]) #エラー確認用に一時的に出力
-                f.write(str(pr_json_data[api_param[value]]))
-            elif isinstance(pr_json_data[api_param[value]],type(None)) != 1: #取ったデータがNULLだった場合
+                str = str + str(pr_json_data[api_param[value]])
+            elif isinstance(pr_json_data[api_param[value]],type(None)) != 1: #取ったデータがNULLじゃない場合
                 #print(pr_json_data[api_param[value]]) #エラー確認用に一時的に出力
-                f.write(pr_json_data[api_param[value]])
-            if i < len(api_param)-1: #最終項目の一つ前までカンマを入れる
-                 f.write(',')
-        f.write('\n')
+                str = str + pr_json_data[api_param[value]]
+            str = str + ','
+        if 'changed_files' in api_param.values():
+            change_file = check_change_file(access_token, github_url, pr_json_data['changed_files'], file_param)
+            for i in range(pr_json_data['changed_files']):
+                for j,key in enumerate(file_param[i]):
+                    str = str + change_file[i][j][key] + ','
+        str = str[:-1] + '\n'
+        f.write(str)
     f.closed
-''''''
-github_url = 'https://api.github.com/repos/%s/%s/pulls/' % (owner, repo) + str(21) #+ '/files'
-payload = {'access_token': access_token}
-res = requests.get(github_url, params=payload)
-pr_json_data = json.loads(res.text)  # 個別PRのjson_dataは辞書型
 
+def check_change_file(access_token,url,file_num,file_param):
+    """リスト型で各リストはファイルの追加・削除の辞書型で返す"""
+    prfile_url = url + '/files'
+    payload = {'access_token': access_token}
+    res = requests.get(prfile_url, params=payload)
+    prfile_json_data = json.loads(res.text)  # 個別PRのjson_dataは辞書型
+    list = []
+    for i in range(1,file_num+1)
+        list.append({file_param{key}:file_param})
+'''
+'''
 #テスト用にファイルに出力
 f = open("output.json", "w")
 json.dump(pr_json_data, f, indent=4, sort_keys=True, separators=(',', ': '))
-''''''
+'''
